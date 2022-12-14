8cc92a692034995eb89ca8964066e3fc8cfe82bc
Vanavara <116945564+Vanavara@users.noreply.github.com>
2022-12-14 15:50:52 +0300

    Delete news directory

--- news/newspaper/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin, sites
-from .models import Post, Category, Comment, Author
-
-admin.site.register(Post)
-admin.site.register(Category)
-admin.site.register(Comment)
-admin.site.register(Author)
-
-# Register your models here.

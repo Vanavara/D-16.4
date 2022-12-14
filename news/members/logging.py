bbf0dfa937feb6a649d1587f3d6a988a79399847
Vanavara <116945564+Vanavara@users.noreply.github.com>
2022-12-14 05:54:36 +0300

    Add files via upload

---
+++ news/members/logging.py
@@ -0,0 +1,22 @@
+# import os
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#
+# LOGGING = {
+#     'version': 1,
+#     'disable_existing_loggers': False,
+#     'handlers':{
+#         'file':{
+#             'level': 'WARNING',
+#             'class': 'logging.FileHandler',
+#             'filename': 'errors.log',
+#         },
+#     },
+#     'loggers': {
+#         'mailings': {
+#             'handlers': ['file'],
+#             'level': 'WARNING',
+#             'propagate': True,
+#         },
+#
+#     },
+# }
